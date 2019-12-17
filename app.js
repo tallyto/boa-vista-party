0d134cf7aa818694fbf8e06a55af9ba5eadc3f6d
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2019-12-17 16:53:57 -0400

    Manipulando banco de dados

--- app.js
+++ app.js
@@ -3,15 +3,50 @@ var handlebars = require("express-handlebars");
 const path = require("path");
 const bodyParser = require("body-parser");
 const Atleticas = require("./route/atleticas");
-const Eventos = require('./route/eventos')
+const Eventos = require("./route/eventos");
 const app = express();
 
+/*
+//Database
+const mongoose = require('mongoose');
+require ("./models/eventoSchema")
+const eventoSchema = mongoose.model("eventos")
+*/
+
+//Middleware
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+/*
+mongoose.connect('mongodb://localhost:27017/party', { useNewUrlParser: true , useUnifiedTopology: true}).then(()=>{
+  console.log("Banco de dados concetado com sucesso!")
+}).catch((error)=>{
+  console.log("Error: " + error)
+})
+
+app.get("/cadastrar", (req, res)=>{
+  const newEvento = {
+    title: "Meu evento",
+    description: "Minha descrição",
+    slug: "evento",
+  }
+  eventoSchema.create(newEvento).then((evento)=>{
+    res.json(evento)
+  }).catch((error)=>{
+    res.json(error)
+  })
+})
+
+app.get("/eventos-fake",async (req, res)=>{
+  const eventosDb = await eventoSchema.find()
+  res.render("eventos-fake", {eventos: eventosDb})
+
+})
+*/
 
 app.get("/", (req, res) => {
   res.render("eventos");
---
+++ models/atleticaSchema.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const eventoSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    default: "Título"
+  },
+  description: {
+    type: String,
+    required: true,
+    default: "Descrição"
+  },
+  slug: {
+    type: String,
+    required: true
+  },
+  imgSrc: {
+    type: String,
+    default: "/img/default.png"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
+mongoose.model("eventos", eventoSchema);
