771b5ed47da7f12e11de2aa85d49cc0f5e17b115
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2019-12-19 17:34:10 -0400

    Ajustando caminho de upload da imagem

--- config/multer.js
+++ config/multer.js
@@ -3,10 +3,10 @@ const path = require("path");
 const crypto = require("crypto");
 
 module.exports = {
-  dest: path.resolve(__dirname, "public", "uploads"),
+  dest: path.resolve(__dirname, "..", "..", "public", "uploads"),
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, path.resolve(__dirname, "public", "uploads"));
+      cb(null, path.resolve(__dirname, "..", "..", "public", "uploads"));
     },
     filename: (req, file, cb) => {
       crypto.randomBytes(16, (err, hash) => {
@@ -21,12 +21,7 @@ module.exports = {
     fileSize: 2 * 1024 * 1024
   },
   fileFilter: (req, file, cb) => {
-    const allowedMimes = [
-      "image/jpeg",
-      "image/png",
-      "image/gif",
-      "image/jpg"
-    ];
+    const allowedMimes = ["image/jpeg", "image/png", "image/gif", "image/jpg"];
 
     if (allowedMimes.includes(file.mimetype)) {
       cb(null, true);
