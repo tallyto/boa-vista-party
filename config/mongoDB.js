2014b1f587d27b12e57873eb11cc68cc1f1cebbe
otallyto <rodrigues.tallyto@hotmail.com>
2021-10-19 20:59:07 -0300

    chore: remove mongo url

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 class MongoDB {
   constructor() {
     this.dev = 'mongodb://localhost:27017/party';
-    this.production = 'mongodb+srv://otallyto:Rodrigues_2019@cluster0-jq9ag.mongodb.net/party?retryWrites=true&w=majority';
+    this.production = 'your_mongo_url';
   }
 
   ConnectProduction() {
